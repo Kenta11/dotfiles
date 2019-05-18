4df400a27f27a85195b9d3d2b7fadc560aa74715
Kenta Arai <kenta@unknown>
2019-05-19 01:08:19 +0900

    fix bug of git-submodule

--- setup.bat
+++ setup.bat
@@ -1,5 +1,14 @@
 @rem Vim
+if exist %HOMEPATH%\_vimrc (goto VIMRC_TRUE) else goto VIMRC_FALSE
+:VIMRC_TRUE
 mklink    %HOMEPATH%\_vimrc   %~dp0.vimrc
+:VIMRC_FALSE
+echo "%HOMEPATH%\_vimrc already exists."
+
+if exist %HOMEPATH%\_vimrc (goto VIM_TRUE) else goto VIM_FALSE
+:VIM_TRUE
 mklink /J %HOMEPATH%\vimfiles %~dp0.vim
+:VIM_FALSE
+echo "%HOMEPATH%\_vim already exists."
 
 git submodule update --init --recursive
--- setup.sh
+++ setup.sh
@@ -3,10 +3,24 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # Vim
-ln -sf `echo ${SCRIPT_DIR}/.vimrc` ~/.vimrc
-ln -sf `echo ${SCRIPT_DIR}/.vim` ~/.vim
+if [ ! -e ~/.vimrc ]; then
+    ln -sf ${SCRIPT_DIR}/.vimrc ~/.vimrc
+else
+    echo "~/.vimrc already exists"
+fi
+if [ ! -e ~/.vim ]; then
+    ln -sf ${SCRIPT_DIR}/.vim ~/.vim
+else
+    echo "~/.vim already exists"
+fi
+
 
 # Zsh
-ln -sf `echo ${SCRIPT_DIR}/.zshrc` ~/.zshrc
+if [ ! -e ~/.zshrc ]; then
+    ln -sf ${SCRIPT_DIR}/.zshrc ~/.zshrc
+else
+    echo "~/.zshrc already exists"
+fi
+
 
 git submodule update --init --recursive
